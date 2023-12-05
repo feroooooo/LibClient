cd7b3a81ae1b4e1b563bd8277e545a592e71d3a8
feroooooo <1799363617@qq.com>
2023-12-05 20:32:34 +0800

    update ui

--- Ui_login.py
+++ Ui_login.py
@@ -15,28 +15,29 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
-    QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLayout,
+    QLineEdit, QSizePolicy, QVBoxLayout, QWidget)
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        if not Form.objectName():
-            Form.setObjectName(u"Form")
-        Form.resize(400, 300)
-        self.verticalLayoutWidget = QWidget(Form)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(0, 90, 401, 211))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+class Ui_LoginForm(object):
+    def setupUi(self, LoginForm):
+        if not LoginForm.objectName():
+            LoginForm.setObjectName(u"LoginForm")
+        LoginForm.resize(1280, 720)
+        LoginForm.setMinimumSize(QSize(960, 540))
+        LoginForm.setMaximumSize(QSize(1280, 720))
+        self.verticalLayout_2 = QVBoxLayout(LoginForm)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.frame = QFrame(LoginForm)
+        self.frame.setObjectName(u"frame")
+        self.frame.setStyleSheet(u"")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout = QVBoxLayout(self.frame)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.label = QLabel(self.verticalLayoutWidget)
-        self.label.setObjectName(u"label")
-
-        self.horizontalLayout.addWidget(self.label)
-
-        self.lineEdit = QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit = QLineEdit(self.frame)
         self.lineEdit.setObjectName(u"lineEdit")
 
         self.horizontalLayout.addWidget(self.lineEdit)
@@ -46,12 +47,7 @@ class Ui_Form(object):
 
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.label_3 = QLabel(self.verticalLayoutWidget)
-        self.label_3.setObjectName(u"label_3")
-
-        self.horizontalLayout_4.addWidget(self.label_3)
-
-        self.lineEdit_2 = QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_2 = QLineEdit(self.frame)
         self.lineEdit_2.setObjectName(u"lineEdit_2")
 
         self.horizontalLayout_4.addWidget(self.lineEdit_2)
@@ -61,12 +57,7 @@ class Ui_Form(object):
 
         self.horizontalLayout_5 = QHBoxLayout()
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.label_2 = QLabel(self.verticalLayoutWidget)
-        self.label_2.setObjectName(u"label_2")
-
-        self.horizontalLayout_5.addWidget(self.label_2)
-
-        self.lineEdit_3 = QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_3 = QLineEdit(self.frame)
         self.lineEdit_3.setObjectName(u"lineEdit_3")
 
         self.horizontalLayout_5.addWidget(self.lineEdit_3)
@@ -75,15 +66,19 @@ class Ui_Form(object):
         self.verticalLayout.addLayout(self.horizontalLayout_5)
 
 
-        self.retranslateUi(Form)
+        self.verticalLayout_2.addWidget(self.frame)
 
-        QMetaObject.connectSlotsByName(Form)
+
+        self.retranslateUi(LoginForm)
+
+        QMetaObject.connectSlotsByName(LoginForm)
     # setupUi
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.label.setText(QCoreApplication.translate("Form", u"TextLabel", None))
-        self.label_3.setText(QCoreApplication.translate("Form", u"TextLabel", None))
-        self.label_2.setText(QCoreApplication.translate("Form", u"TextLabel", None))
+    def retranslateUi(self, LoginForm):
+        LoginForm.setWindowTitle(QCoreApplication.translate("LoginForm", u"\u767b\u5f55", None))
+        self.lineEdit.setText("")
+        self.lineEdit.setPlaceholderText(QCoreApplication.translate("LoginForm", u"\u5b66\u53f7", None))
+        self.lineEdit_2.setPlaceholderText(QCoreApplication.translate("LoginForm", u"\u5bc6\u7801\uff08\u8eab\u4efd\u8bc1\u540e6\u4f4d\uff09", None))
+        self.lineEdit_3.setPlaceholderText(QCoreApplication.translate("LoginForm", u"\u9a8c\u8bc1\u7801", None))
     # retranslateUi
 
